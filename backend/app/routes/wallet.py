d700d7f45f25079175e0678aa15da1e9e7180338
rooshekky <rooshekky@gmail.com>
2026-03-08 18:18:27 -0700

    commit to main

--- backend/app/routes/wallet.py
+++
@@ -1,9 +0,0 @@
-
-from fastapi import APIRouter
-from ..wallet_engine import create_wallet
-
-router = APIRouter(prefix="/wallet")
-
-@router.get("/create")
-def new_wallet():
-    return create_wallet()
