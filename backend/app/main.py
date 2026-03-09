042c8f4165a987b2a8b33d058122e110242135e2
rooshekky <rooshekky@gmail.com>
2026-03-08 17:47:53 -0700

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,3 +12,10 @@ app.include_router(wallet.router)
 @app.get("/")
 def root():
     return {"wallet":"running"}
+
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("backend.app.main:app", host="0.0.0.0", port=port)
